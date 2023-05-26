0c721cf4b4cb1d58195cbe0869324fdb9bf1fce0
IndraMaulanaPratama <maulana.indra808@gmail.com>
2023-05-26 13:36:51 +0700

    DB SEEDER | Menyesuaikan kebutuhan menu dan submenu

--- database/seeders/MenuSeeder.php
+++ database/seeders/MenuSeeder.php
@@ -3,7 +3,6 @@
 namespace Database\Seeders;
 
 use App\Models\menu;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Carbon;
 
@@ -15,12 +14,46 @@ class MenuSeeder extends Seeder
     public function run(): void
     {
         menu::insert([
+          // Postingan
+          [
+            'MENU_ORDER' => 1,
+            'MENU_NAME' => 'Postingan',
+            'MENU_URL' => 'postingan',
+            'MENU_ICON' => 'fa fe-trello',
+
+            'MENU_CREATED_AT' => Carbon::now('Asia/Jakarta'),
+            'MENU_CREATED_BY' => 1,
+          ],
+
+          // Pengguna
+          [
+            'MENU_ORDER' => 2,
+            'MENU_NAME' => 'Pengguna',
+            'MENU_URL' => 'pengguna',
+            'MENU_ICON' => 'fe fe-users',
+
+            'MENU_CREATED_AT' => Carbon::now('Asia/Jakarta'),
+            'MENU_CREATED_BY' => 1,
+          ],
+
+          // Menu
+          [
+            'MENU_ORDER' => 3,
+            'MENU_NAME' => 'Menu',
+            'MENU_URL' => 'menu',
+            'MENU_ICON' => 'fe fe-menu',
+
+            'MENU_CREATED_AT' => Carbon::now('Asia/Jakarta'),
+            'MENU_CREATED_BY' => 1,
+          ],
+
+
           // Setting
           [
             'MENU_ORDER' => 99,
             'MENU_NAME' => 'Setting',
             'MENU_URL' => 'setting',
-            'MENU_ICON' => 'fa fa-cogs',
+            'MENU_ICON' => 'fe fe-settings',
 
             'MENU_CREATED_AT' => Carbon::now('Asia/Jakarta'),
             'MENU_CREATED_BY' => 1,
--- database/seeders/SubmenuSeeder.php
+++ database/seeders/SubmenuSeeder.php
@@ -3,7 +3,6 @@
 namespace Database\Seeders;
 
 use App\Models\submenu;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Carbon;
 
@@ -15,9 +14,79 @@ class SubmenuSeeder extends Seeder
     public function run(): void
     {
         submenu::insert([
-          // Setting -> Testimoni
+
+          // Postingan Area //
+          // Postingan -> Buat Postingan
           [
             'SUBMENU_MENU' => 1,
+            'SUBMENU_ORDER' => 1,
+            'SUBMENU_NAME' => 'Buat Postingan',
+            'SUBMENU_URL' => 'postingan/create',
+
+            'SUBMENU_CREATED_AT' => Carbon::now('Asia/Jakarta'),
+            'SUBMENU_CREATED_BY' => 1,
+          ],
+
+          // Postingan -> Daftar Postingan
+          [
+            'SUBMENU_MENU' => 1,
+            'SUBMENU_ORDER' => 2,
+            'SUBMENU_NAME' => 'Daftar Postingan',
+            'SUBMENU_URL' => 'postingan',
+
+            'SUBMENU_CREATED_AT' => Carbon::now('Asia/Jakarta'),
+            'SUBMENU_CREATED_BY' => 1,
+          ],
+
+          // Postingan -> Daftar Kategori
+          [
+            'SUBMENU_MENU' => 1,
+            'SUBMENU_ORDER' => 3,
+            'SUBMENU_NAME' => 'Daftar Kategori',
+            'SUBMENU_URL' => 'kategori',
+
+            'SUBMENU_CREATED_AT' => Carbon::now('Asia/Jakarta'),
+            'SUBMENU_CREATED_BY' => 1,
+          ],
+
+          // Postingan -> Data Sampah
+          [
+            'SUBMENU_MENU' => 1,
+            'SUBMENU_ORDER' => 4,
+            'SUBMENU_NAME' => 'Data Sampah',
+            'SUBMENU_URL' => 'postingan/trash',
+
+            'SUBMENU_CREATED_AT' => Carbon::now('Asia/Jakarta'),
+            'SUBMENU_CREATED_BY' => 1,
+          ],
+
+          // Pengguna Area //
+          // Pengguna -> Daftar Pengguna
+          [
+            'SUBMENU_MENU' => 2,
+            'SUBMENU_ORDER' => 1,
+            'SUBMENU_NAME' => 'Daftar Pengguna',
+            'SUBMENU_URL' => 'pengguna',
+
+            'SUBMENU_CREATED_AT' => Carbon::now('Asia/Jakarta'),
+            'SUBMENU_CREATED_BY' => 1,
+          ],
+
+          // Pengguna -> Role Pengguna
+          [
+            'SUBMENU_MENU' => 2,
+            'SUBMENU_ORDER' => 2,
+            'SUBMENU_NAME' => 'Role Pengguna',
+            'SUBMENU_URL' => 'role',
+
+            'SUBMENU_CREATED_AT' => Carbon::now('Asia/Jakarta'),
+            'SUBMENU_CREATED_BY' => 1,
+          ],
+
+          // Setting Area //
+          // Setting -> Testimoni
+          [
+            'SUBMENU_MENU' => 4,
             'SUBMENU_ORDER' => 1,
             'SUBMENU_NAME' => 'Testimoni',
             'SUBMENU_URL' => 'setting/testimoni',
@@ -28,7 +97,7 @@ class SubmenuSeeder extends Seeder
 
           // Setting -> Contact
           [
-            'SUBMENU_MENU' => 1,
+            'SUBMENU_MENU' => 4,
             'SUBMENU_ORDER' => 2,
             'SUBMENU_NAME' => 'Contact',
             'SUBMENU_URL' => 'setting/contact',
