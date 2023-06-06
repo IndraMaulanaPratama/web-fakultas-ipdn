259d3e26d0110ecd0b42f8c385547c94f94e3f18
IndraMaulanaPratama <maulana.indra808@gmail.com>
2023-06-06 09:51:43 +0700

    ADD NEW | Component data table for showing data postingan

---
+++ app/Http/Livewire/Component/Admin/ArticleManager/DataTable.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Component\Admin\ArticleManager;
+
+use Livewire\Component;
+
+class DataTable extends Component
+{
+    public function render()
+    {
+        return view('livewire.component.admin.article-manager.data-table');
+    }
+}
