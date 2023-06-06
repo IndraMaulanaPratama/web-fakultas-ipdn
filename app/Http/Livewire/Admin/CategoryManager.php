73ab738a8879fd8ae7a9a84d34c20e68eaff0251
IndraMaulanaPratama <maulana.indra808@gmail.com>
2023-06-06 08:52:48 +0700

    UPDATE | Refatoring logic render and handle form add or update

--- app/Http/Livewire/Admin/CategoryManager.php
+++ app/Http/Livewire/Admin/CategoryManager.php
@@ -6,12 +6,24 @@ use App\Models\category;
 use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class CategoryManager extends Component
 {
+    use WithPagination;
+
+    public $category;
     public $login;
+    public $formOption;
+
     public $inputCategory;
-    public $formOption = false;
+    public $inputSort = null;
+    public $inputSearch;
+
+
+    protected $paginationTheme = 'bootstrap';
+    protected $sortField = 'CATEGORY_CREATED_AT';
+    protected $sortOption = 'DESC';
 
     protected $listeners = [
       'responseAddNew',
@@ -22,29 +34,51 @@ class CategoryManager extends Component
     public function mount()
     {
         $this->login = Auth::user();
+        $this->formOption = 'add';
+    }
+
+    public function update()
+    {
+        if ('asc_name' == $this->inputSort) :
+            $this->sortField = 'CATEGORY_NAME';
+            $this->sortOption = 'ASC';
+
+        elseif('desc_name' == $this->inputSort):
+            $this->sortField = 'CATEGORY_NAME';
+            $this->sortOption = 'DESC';
+
+        elseif('asc_date' == $this->inputSort):
+            $this->sortField = 'CATEGORY_CREATED_AT';
+            $this->sortOption = 'ASC';
+
+        elseif('desc_date' == $this->inputSort || null == $this->inputSort):
+            $this->sortField = 'CATEGORY_CREATED_AT';
+            $this->sortOption = 'DESC';
+        endif;
     }
 
     public function responseAddNew($data)
     {
+        $this->formOption = 'add';
         session()->flash($data['status'], $data['message']);
     }
 
     public function FormUpdate($id)
     {
-        $this->formOption = true;
+        $this->formOption = 'update';
         $data =  category::find($id);
         $this->emit('showFormUpdate', $data);
     }
 
     public function responseUpdate($data)
     {
-      $this->formOption = false;
-      session()->flash($data['status'], $data['message']);
+        $this->formOption = 'add';
+        session()->flash($data['status'], $data['message']);
     }
 
     public function cancelUpdate()
     {
-        $this->formOption = false;
+        $this->formOption = 'add';
     }
 
     public function SoftDelete($id)
@@ -66,31 +100,42 @@ class CategoryManager extends Component
 
             if ($category->save()) : // Step 2
                 // Step 3
+                $this->formOption = 'add';
                 session()->flash('success', 'Kategori yang anda pilih berhasil dihapuskan');
             endif;
 
         } catch (\Throwable $th) { // Step 4
+            $this->formOption = 'add';
             session()->flash('error', 'Kategori yang anda pilih gagal dihapuskan');
         }
     }
 
     public function render()
     {
-        $category = category::with('user')->NullDeleted()->get([
-          'CATEGORY_ID AS ID',
-          'CATEGORY_NAME AS NAME',
-          'CATEGORY_CREATED_AT AS CREATED_DATE',
-          'CATEGORY_CREATED_BY'
-        ]);
-
         $params = [
           'title' => 'Daftar Kategori - IPDN Kampus Daerah',
           'page_name' => 'Daftar Kategori',
         ];
 
-        return view('livewire.admin.category-manager', ['data' => $category])
+        return view(
+            'livewire.admin.category-manager',
+            [
+              'paginate' => category::with('user')
+              ->NullDeleted()
+              ->where('CATEGORY_NAME', 'LIKE', '%'. $this->inputSearch .'%')
+              ->orderBy($this->sortField, $this->sortOption)
+              ->paginate(
+                  15,
+                  [
+                'CATEGORY_ID AS ID',
+                'CATEGORY_NAME AS NAME',
+                'CATEGORY_CREATED_AT AS CREATED_DATE',
+                'CATEGORY_CREATED_BY'
+                ]
+              )
+            ]
+        )
         ->extends('layouts.admin', $params)
         ->section('content');
-
     }
 }
