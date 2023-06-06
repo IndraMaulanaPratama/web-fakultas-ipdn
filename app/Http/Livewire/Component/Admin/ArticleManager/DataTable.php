9b9c522f23e4826e5e63b5d7a263311dbf279180
IndraMaulanaPratama <maulana.indra808@gmail.com>
2023-06-06 14:16:23 +0700

    ADD NEW | Create Full Function of Data Table for article

--- app/Http/Livewire/Component/Admin/ArticleManager/DataTable.php
+++ app/Http/Livewire/Component/Admin/ArticleManager/DataTable.php
@@ -2,12 +2,90 @@
 
 namespace App\Http\Livewire\Component\Admin\ArticleManager;
 
+use App\Models\article;
+use App\Models\category;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class DataTable extends Component
 {
+    use WithPagination;
+
+    public $inputSort = null;
+    public $inputStatus = null;
+    public $inputCategory = null;
+    public $inputSearch = null;
+
+    protected $paginationTheme = 'bootstrap';
+    protected $sortField = 'ARTICLE_CREATED_AT';
+    protected $sortOption = 'DESC';
+    protected $selectCategory;
+
+    public function mount()
+    {
+    }
+
+    public function update()
+    {
+        // Handle for Input Sort
+        if ('asc_title' == $this->inputSort) :
+            $this->sortField = 'ARTICLE_TITLE';
+            $this->sortOption = 'ASC';
+
+        elseif ('desc_title' == $this->inputSort) :
+            $this->sortField = 'ARTICLE_TITLE';
+            $this->sortOption = 'DESC';
+
+        elseif ('asc_date' == $this->inputSort) :
+            $this->sortField = 'ARTICLE_CREATED_AT';
+            $this->sortOption = 'ASC';
+
+        elseif ('desc_date' == $this->inputSort || null == $this->inputSort) :
+            $this->sortField = 'ARTICLE_CREATED_AT';
+            $this->sortOption = 'DESC';
+        endif;
+    }
+
     public function render()
     {
-        return view('livewire.component.admin.article-manager.data-table');
+        return view(
+            'livewire.component.admin.article-manager.data-table',
+            [
+              'paginate' => article::with('category')
+              ->nullDeleted()
+              ->where(function ($query) {
+
+                  // Handle Filter by Status
+                  if (null != $this->inputStatus) :
+                      $query->status($this->inputStatus);
+                  endif;
+
+                  // Handle Filter by Kategori
+                  if(null != $this->inputCategory) :
+                      $query->category($this->inputCategory);
+                  endif;
+              })
+              // ->status($this->inputStatus)
+              // ->category($this->inputCategory)
+              ->searchTitle($this->inputSearch)
+              ->orderBy($this->sortField, $this->sortOption)
+              ->paginate(
+                  15,
+                  [
+                'ARTICLE_ID AS ID',
+                'ARTICLE_TITLE',
+                'ARTICLE_CATEGORY',
+                'ARTICLE_STATUS',
+                'ARTICLE_CREATED_AT AS CREATED_DATE',
+              ]
+              ),
+                  'category' => category::nullDeleted()->get(
+                      [
+                    'CATEGORY_ID',
+                    'CATEGORY_NAME'
+                  ]
+                  ),
+        ]
+        );
     }
 }
--- resources/views/livewire/component/admin/article-manager/data-table.blade.php
+++ resources/views/livewire/component/admin/article-manager/data-table.blade.php
@@ -1,3 +1,103 @@
+{{-- The Master doesn't talk, he acts. --}}
 <div>
-    {{-- The Master doesn't talk, he acts. --}}
+
+    {{-- Settings Data Table Area --}}
+    <div class="row">
+
+        {{-- Sort By --}}
+        <div class="col-md-2">
+            <select wire:model="inputSort" class="form-control" id="inputSort">
+                <option value="">{{ __('Urutan Data:') }}</option>
+                <option value="asc_title">{{ __('Judul A-Z') }}</option>
+
+                <option value="desc_title">{{ __('Judul Z-A') }}</option>
+
+                <option value="asc_date">{{ __('Tanggal Dibuat A-Z') }}</option>
+
+                <option value="desc_date">{{ __('Tanggal Dibuat Z-A') }}</option>
+
+            </select>
+        </div>
+
+        {{-- Filter Status --}}
+        <div class="col-md-2">
+            <select wire:model="inputStatus" class="form-control" id="inputStatus">
+                <option value="">{{ __('Status:') }}</option>
+                <option value="">{{ __('Semua Data') }}</option>
+                <option value="1">{{ __('Tayang') }}</option>
+                <option value="2">{{ __('Draft') }}</option>
+            </select>
+        </div>
+
+        {{-- Filter Category --}}
+        <div class="col-md-5">
+            {{-- {{ $category[0]->CATEGORY_ID }} --}}
+            <select wire:model="inputCategory" class="form-control col-md-4" id="inputCategory">
+                <option value="">{{ __('Kategori:') }}</option>
+                <option value="">{{ __('Semua Data') }}</option>
+                @foreach ($category as $item)
+                    <option value="{{ $item->CATEGORY_ID }}">{{ $item->CATEGORY_NAME }}</option>
+                @endforeach
+
+            </select>
+        </div>
+
+        {{-- Search Input --}}
+        <div class="col-md-3">
+            <div class="input-group">
+                <div class="input-group-prepend">
+                    <span class="input-group-text fe fe-search" id="inputGroupPrepend"></span>
+                </div>
+                <input type="text" wire:model='inputSearch' class="form-control" id="validationCustomUsername"
+                    aria-describedby="inputGroupPrepend">
+            </div>
+        </div>
+    </div>
+
+    <div class="mb-4">&nbsp;</div>
+
+    <div class="row">
+        <div class="col-md-12">
+            <table class="table table-hover" id="dataTable-category">
+                <thead>
+                    <tr>
+                        <th>{{ __('Judul Postingan') }}</th>
+                        <th>{{ __('Kategori') }}</th>
+                        <th>{{ __('Status') }}</th>
+                        {{-- <th>{{ __('Dibuat Oleh') }}</th> --}}
+                        <th>{{ __('Tanggal Pembuatan') }}</th>
+                        <th>{{ __('Action') }}</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($paginate as $data)
+                        <tr>
+                            <td>{{ $data->ARTICLE_TITLE }}</td>
+                            <td>{{ $data->category['CATEGORY_NAME'] }}</td>
+                            <td>{{ 1 == $data->ARTICLE_STATUS ? 'Tayang' : 'Draft' }}</td>
+                            {{-- <td>{{ $data->user['name'] }}</td> --}}
+                            <td>{{ $data->CREATED_DATE }}</td>
+                            <td><button class="btn btn-sm dropdown-toggle more-horizontal" type="button"
+                                    data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    <span class="text-muted sr-only">{{ __('Action') }}</span>
+                                </button>
+                                <div class="dropdown-menu dropdown-menu-right">
+                                    <button class="dropdown-item"
+                                        wire:click='FormUpdate({{ $data->ID }})'>{{ __('Edit') }}
+                                    </button>
+                                    <button class="dropdown-item"
+                                        wire:click='SoftDelete({{ $data->ID }})'>{{ __('Remove') }}
+                                    </button>
+                                </div>
+                            </td>
+                        </tr>
+                    @endforeach
+
+                </tbody>
+            </table>
+
+            <hr class="pb-5">
+            {{ $paginate->links() }}
+        </div>
+    </div>
 </div>
