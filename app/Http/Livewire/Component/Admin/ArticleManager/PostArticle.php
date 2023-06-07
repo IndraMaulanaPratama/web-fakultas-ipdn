cc0c011a3b13c56b6461a7c6bcc7ea10dfa0a371
IndraMaulanaPratama <maulana.indra808@gmail.com>
2023-06-07 09:40:51 +0700

    ADD NEW | Skeleton for logic post article

---
+++ app/Http/Livewire/Component/Admin/ArticleManager/PostArticle.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Component\Admin\ArticleManager;
+
+use Livewire\Component;
+
+class PostArticle extends Component
+{
+    public function render()
+    {
+        return view('livewire.component.admin.article-manager.post-article');
+    }
+}
