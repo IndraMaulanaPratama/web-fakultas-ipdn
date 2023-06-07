9ea7b4f0322c40cb5b52f552e983782a193c0763
IndraMaulanaPratama <maulana.indra808@gmail.com>
2023-06-07 09:41:23 +0700

    ADD NEW | Template view for post article

---
+++ resources/views/livewire/admin/post-article.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.admin', [
+    'title' => 'Formulir Postingan - IPDN Kampus Daerah',
+    'page_name' => 'Formulir Pembuatan Postingan Baru',
+]);
+
+@push('style')
+    <script src="https://cdn.ckeditor.com/4.21.0/standard/ckeditor.js"></script>
+@endpush
+
+@section('content')
+    @livewire('component.admin.article-manager.post-article')
+@endsection
+
+@push('script')
+<script>
+  CKEDITOR.replace( 'inputContent' );
+</script>
+@endpush
