7e0b394abbd32630bbf21e77ebb239dd8bce90da
IndraMaulanaPratama <maulana.indra808@gmail.com>
2023-06-07 09:42:31 +0700

    ADD NEW | Skeleton for logic update data article

---
+++ app/Http/Livewire/Component/Admin/ArticleManager/EditArticle.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Component\Admin\ArticleManager;
+
+use Livewire\Component;
+
+class EditArticle extends Component
+{
+    public function render()
+    {
+        return view('livewire.component.admin.article-manager.edit-article');
+    }
+}
