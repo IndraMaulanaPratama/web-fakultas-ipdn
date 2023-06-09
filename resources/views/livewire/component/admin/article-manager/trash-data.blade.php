af3a1fec14d7ee62230b3d73f0ed287330b91cb8
IndraMaulanaPratama <maulana.indra808@gmail.com>
2023-06-09 14:33:26 +0700

    ADD NEW | Data Table Category on trash data page view

--- resources/views/livewire/component/admin/article-manager/trash-data.blade.php
+++ resources/views/livewire/component/admin/article-manager/trash-data.blade.php
@@ -28,11 +28,14 @@
         </div>
     @endif
 
+    {{-- Data Article --}}
     <div class="row">
         <div class="col-md-12">
             <div class="accordion accordion-boxed" id="accordion2">
+
                 {{-- Data Article --}}
                 <div class="card shadow">
+
                     {{-- Header --}}
                     <div class="card-header" id="headingOne">
                         <a role="button" href="#collapseOne" data-toggle="collapse" data-target="#collapseOne"
@@ -112,7 +115,7 @@
                                             </tr>
                                         </thead>
                                         <tbody>
-                                            @foreach ($paginate as $data)
+                                            @foreach ($paginate_article as $data)
                                                 <tr>
                                                     <td>{{ $data->ARTICLE_TITLE }}</td>
                                                     <td>{{ $data->category['CATEGORY_NAME'] }}</td>
@@ -141,12 +144,25 @@
                                     </table>
 
                                     <hr class="pb-5">
-                                    {{ $paginate->links() }}
+                                    {{ $paginate_article->links() }}
                                 </div>
                             </div>
+
                         </div>
                     </div>
+
                 </div>
+
+            </div>
+        </div>
+    </div>
+
+    <div class="">&nbsp;</div>
+
+    {{-- Data Category --}}
+    <div class="row">
+        <div class="col-md-12">
+            <div class="accordion accordion-boxed" id="accordion1">
 
                 {{-- Data Category --}}
                 <div class="card shadow">
@@ -156,17 +172,90 @@
                             <strong>{{ __('Daftar Data Kategori') }}</strong>
                         </a>
                     </div>
-                    <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordion2">
-                        <div class="card-body"> Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus
-                            terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch.
-                            Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a
-                            bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh
-                            helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan
-                            excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic
-                            synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS. </div>
+                    <div id="collapseTwo" class="collapse show" aria-labelledby="headingTwo"
+                        data-parent="#accordion1">
+                        <div class="card-body">
+
+                            {{-- Data Table Area --}}
+                            <div class="row">
+                                <div class="col-md-9">
+                                    <select wire:model="inputSortCategory" class="form-control col-md-3"
+                                        id="inputSort">
+                                        <option value="">Urutan Data:</option>
+                                        <option value="asc_name">{{ __('Nama A-Z') }}</option>
+
+                                        <option value="desc_name">{{ __('Nama Z-A') }}</option>
+
+                                        <option value="asc_date">{{ __('Tanggal Dibuat A-Z') }}</option>
+
+                                        <option value="desc_date">{{ __('Tanggal Dibuat Z-A') }}</option>
+
+                                    </select>
+                                </div>
+
+                                <div class="col-md-3">
+                                    <div class="input-group">
+                                        <div class="input-group-prepend">
+                                            <span class="input-group-text fe fe-search" id="inputGroupPrepend"></span>
+                                        </div>
+                                        <input type="text" wire:model='inputSearchCategory' class="form-control"
+                                            id="validationCustomUsername" aria-describedby="inputGroupPrepend">
+                                    </div>
+                                </div>
+                            </div>
+
+                            {{-- Data Table --}}
+                            <div class="row mt-4">
+                                <div class="col-md-12">
+                                    <table class="table table-hover" id="dataTable-category">
+                                        <thead>
+                                            <tr>
+                                                <th>{{ __('Nama Kategori') }}</th>
+                                                <th>{{ __('Dibuat Oleh') }}</th>
+                                                <th>{{ __('Tanggal Pembuatan') }}</th>
+                                                <th>{{ __('Action') }}</th>
+                                            </tr>
+                                        </thead>
+
+                                        <tbody>
+                                            @foreach ($paginate_category as $data)
+                                                <tr>
+                                                    <td>{{ $data->NAME }}</td>
+                                                    <td>{{ $data->user['name'] }}</td>
+                                                    <td>{{ $data->CREATED_DATE }}</td>
+                                                    <td><button class="btn btn-sm dropdown-toggle more-horizontal"
+                                                            type="button" data-toggle="dropdown"
+                                                            aria-haspopup="true" aria-expanded="false">
+                                                            <span
+                                                                class="text-muted sr-only">{{ __('Action') }}</span>
+                                                        </button>
+                                                        <div class="dropdown-menu dropdown-menu-right">
+                                                            <button class="dropdown-item"
+                                                                wire:click='FormUpdate({{ $data->ID }})'>{{ __('Edit') }}
+                                                            </button>
+                                                            <button class="dropdown-item"
+                                                                wire:click='SoftDelete({{ $data->ID }})'>{{ __('Remove') }}
+                                                            </button>
+                                                        </div>
+                                                    </td>
+                                                </tr>
+                                            @endforeach
+
+                                        </tbody>
+
+                                    </table>
+
+                                    <hr class="pb-5">
+                                    {{ $paginate_category->links() }}
+                                </div>
+                            </div>
+
+                        </div>
                     </div>
                 </div>
+
             </div>
         </div>
     </div>
+
 </div>
