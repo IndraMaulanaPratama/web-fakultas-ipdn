3ffb620dc67bdb9879e3fa81c466dde494cc98c9
IndraMaulanaPratama <maulana.indra808@gmail.com>
2023-06-12 09:39:52 +0700

    ADD NEW | Config for storing data image thumbnail article

--- config/filesystems.php
+++ config/filesystems.php
@@ -44,6 +44,15 @@ return [
             'throw' => false,
         ],
 
+        'photo' => [
+          'driver' => 'local',
+          'root' => storage_path('app/public/image/article'),
+          'url' => env('APP_URL').'/storage',
+          'visibility' => 'public',
+          'throw' => false,
+        ],
+
+
         's3' => [
             'driver' => 's3',
             'key' => env('AWS_ACCESS_KEY_ID'),
