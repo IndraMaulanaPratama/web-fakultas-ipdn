f21db974bcc6a4be9b415f6cf5a990620d65c81e
IndraMaulanaPratama <maulana.indra808@gmail.com>
2023-06-13 17:02:30 +0700

    ADD NEW | Fiture Upload File on logic edit article

--- app/Http/Livewire/Component/Admin/ArticleManager/EditArticle.php
+++ app/Http/Livewire/Component/Admin/ArticleManager/EditArticle.php
@@ -7,17 +7,27 @@ use App\Models\category;
 use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
+use Livewire\WithFileUploads;
 
 class EditArticle extends Component
 {
+    use WithFileUploads;
+
     public $inputTitle;
     public $inputContent;
     public $inputCategory;
+    public $inputThumbnail;
     public $idArticle;
     public $articleCategory;
 
     public $article;
     public $category;
+
+    protected $rules = [
+      'inputTitle' => ['required', 'max:150'],
+      'inputContent' => ['required'],
+      'inputThumbnail' => ['image', 'max:2048'],
+    ];
 
     public function mount($id)
     {
@@ -31,34 +41,51 @@ class EditArticle extends Component
         $this->category = category::nullDeleted()->get(['CATEGORY_ID', 'CATEGORY_NAME']);
     }
 
-    public function updated()
+    public function updated($propertyName)
     {
+        $this->validateOnly($propertyName);
     }
 
     public function updateArticle()
     {
         // Validation data mandatory
-        $this->validate([
-          'inputTitle' => ['required'],
-          'inputContent' => ['required'],
-          'inputCategory' => ['required'],
-        ]);
+        $this->validate();
+
+        // Inisialisasi Variable
+        $filename = $this->inputThumbnail->getClientOriginalName();
 
         // Initialization update data article
         $this->article->ARTICLE_TITLE = $this->inputTitle;
         $this->article->ARTICLE_CONTENT = $this->inputContent;
         $this->article->ARTICLE_CATEGORY = $this->inputCategory;
+        $this->article->ARTICLE_THUMBNAIL = $filename;
         $this->article->ARTICLE_UPDATED_AT = Carbon::now('Asia/Jakarta');
         $this->article->ARTICLE_UPDATED_BY = Auth::user()->id;
 
         // Logic Handle Update Data
         try {
+            // Upload File
+            if ($this->inputThumbnail->storeAS('photo', $filename)) :
+                # code...
+            endif;
+
+            // Process updated data
             $this->article->save();
 
+            // Throw success message to frontend
             session()->flash('success', 'Data Article Berhasil Diperbaharui');
         } catch (\Throwable $th) {
+            // Throw failed message to frontend
             session()->flash('error', 'Data Article Berhasil Gagal Diperbaharui');
         }
+    }
+
+    public function clearForm()
+    {
+        $this->inputTitle = null;
+        $this->inputContent = null;
+        $this->inputCategory = null;
+        $this->inputThumbnail = null;
     }
 
     public function render()
