40ecc7cfb5a58c11ac4cd9f0f7afe20d046f735e
IndraMaulanaPratama <maulana.indra808@gmail.com>
2023-06-14 15:27:25 +0700

    ADD NEW | Get Data Post From Database on berita logic

--- app/Http/Livewire/Homepage/Berita.php
+++ app/Http/Livewire/Homepage/Berita.php
@@ -2,12 +2,20 @@
 
 namespace App\Http\Livewire\Homepage;
 
+use App\Models\article;
 use Livewire\Component;
 
 class Berita extends Component
 {
+    public $berita;
+
+    public function mount()
+    {
+        $this->berita = article::nullDeleted()->take(3)->orderBy('ARTICLE_ID', 'DESC')->get();
+    }
+
     public function render()
     {
-        return view('livewire.homepage.berita');
+        return view('livewire.homepage.berita', ['data' => $this->berita]);
     }
 }
