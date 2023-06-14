e700848cf4017dd887e32fad57c0f76e7900cc86
IndraMaulanaPratama <maulana.indra808@gmail.com>
2023-06-14 15:28:56 +0700

    ADD NEW | Get Data Article From Database on Homepage Logic

--- app/Http/Livewire/Homepage/Homopage.php
+++ app/Http/Livewire/Homepage/Homopage.php
@@ -2,18 +2,26 @@
 
 namespace App\Http\Livewire\Homepage;
 
+use App\Models\article;
 use Livewire\Component;
 
 class Homopage extends Component
 {
-    public function render()
-    {
-        $params = [
-            'title' => 'Homepage - IPDN Kampus Daerah'
-        ];
+    public $berita;
 
-        return view('livewire.homepage.homopage')
-        ->extends('layouts.public', $params)
-        ->section('content');
+    public function mount()
+    {
+        $this->berita = article::with('user')->take(3)->get();
     }
+
+      public function render()
+      {
+          $params = [
+              'title' => 'Homepage - IPDN Kampus Daerah'
+          ];
+
+          return view('livewire.homepage.homopage', $this->berita)
+          ->extends('layouts.public', $params)
+          ->section('content');
+      }
 }
