c48f5ab7c9966c5019df3702edc6cc6447f459ff
IndraMaulanaPratama <maulana.indra808@gmail.com>
2023-06-15 10:56:29 +0700

    ADD NEW | Handle Post By Category on list berita logic

--- app/Http/Livewire/Category/ListBerita.php
+++ app/Http/Livewire/Category/ListBerita.php
@@ -2,17 +2,36 @@
 
 namespace App\Http\Livewire\Category;
 
+use App\Models\article;
+use App\Models\category;
 use Livewire\Component;
 
 class ListBerita extends Component
 {
+    public $article;
+    public $category;
+    public $categoryId;
+
+    public function mount($categoryId)
+    {
+        $this->categoryId = $categoryId;
+        $this->category = category::find($categoryId);
+    }
+
+
     public function render()
     {
+        $this->article = article::with('user', 'category')->category($this->categoryId)->nullDeleted()->orderBy('ARTICLE_ID', 'DESC')->get();
+
         $params = [
-            'title' => 'Berita Berdasarkan Kategori - IPDN Kampus Papua'
+            'title' => 'Postinagn Kategori '. $this->category->CATEGORY_NAME .' - IPDN Kampus Daerah'
         ];
 
-        return view('livewire.category.list-berita')
+        $data = [
+          'data' => $this->article,
+        ];
+
+        return view('livewire.category.list-berita', $data)
         ->extends('layouts.public', $params)
         ->section('content');
     }
