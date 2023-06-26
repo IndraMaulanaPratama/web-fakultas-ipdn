d5e770ddcde1d6b18385a2db6f8a93c474b96c6c
IndraMaulanaPratama <maulana.indra808@gmail.com>
2023-06-26 16:06:43 +0700

    UPDATE || Implement Responsive Data on Page Director Homepage

--- resources/views/livewire/homepage/direktur.blade.php
+++ resources/views/livewire/homepage/direktur.blade.php
@@ -1,23 +1,28 @@
 <div>
     <div class="hero bg-base-100 max-xs:max-w-xs max-sm:max-w-sm max-md:max-w-md">
         <div class="hero-content flex-col lg:flex-row">
-            <img src="https://kalbar.ipdn.ac.id/wp-content/uploads/2022/08/asharizmanrozie.png" class="rounded-lg" />
+            <img src="{{ asset('image/director') . '/' . $setting->SETTING_DIRECTOR_IMAGE }}"
+                alt="{{ $setting->SETTING_DIRECTOR_NAME }}" />
             <div>
 
                 <div class="flex max-xl:justify-center mb-2">
-                    <img src="https://kalbar.ipdn.ac.id/wp-content/uploads/2022/06/photo_2022-06-03_20-02-57.png"
-                        class="mr-4 w-[10%] h-[100%]" alt="">
+                    <img src="https://ipdn.ac.id/assets/img/ipdn.png" class="mr-4 w-[10%] h-[100%]">
+
                     <div class="grid grid-rows-2">
-                        <div class="md:text-2xl max-sm:text-sm font-semibold">Dr. Drs. H. Asharizman Rozie, M.Si.</div>
-                        <div class="font-light max-sm:text-xs">Direktur IPDN Kampus Daerah Papua</div>
+                        <div class="md:text-2xl max-sm:text-sm font-semibold">
+                            {{ $setting->SETTING_DIRECTOR_NAME }}
+                        </div>
+
+                        <div class="font-light max-sm:text-xs">
+                            {{ __('Direktur IPDN Kampus Daerah ') . $setting->SETTING_REGIONAL }}
+                        </div>
                     </div>
                 </div>
 
                 <div class="divider"></div>
 
-                <p class="max-lg:justify-center text-justify">Selamat Datang di Portal Institut Pemerintahan Dalam
-                    Negeri Kampus Daerah Papua. Lembaga Pendidikan Penyedia Calon Birokrat Pemerintahan yang
-                    Profesional.
+                <p class="max-lg:justify-center text-justify indent-5 ">
+                    {{ __($setting->SETTING_DIRECTOR_MESSAGE) }}
                 </p>
             </div>
         </div>
