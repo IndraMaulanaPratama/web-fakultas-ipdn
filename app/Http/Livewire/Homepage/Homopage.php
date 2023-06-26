26e4b1d86d1bfa6be70deb0929a46f291fb22271
IndraMaulanaPratama <maulana.indra808@gmail.com>
2023-06-26 15:48:00 +0700

    UPDATE || Responsive Regional name by general setting on logic homepage

--- app/Http/Livewire/Homepage/Homopage.php
+++ app/Http/Livewire/Homepage/Homopage.php
@@ -3,25 +3,33 @@
 namespace App\Http\Livewire\Homepage;
 
 use App\Models\article;
+use App\Models\setting;
 use Livewire\Component;
 
 class Homopage extends Component
 {
     public $berita;
+    public $setting;
 
     public function mount()
     {
+        $this->setting = setting::find('1');
         $this->berita = article::with('user')->take(3)->get();
     }
 
-      public function render()
-      {
-          $params = [
-              'title' => 'Homepage - IPDN Kampus Daerah'
-          ];
+    public function render()
+    {
+        $params = [
+            'title' => 'Homepage - IPDN Kampus Daerah ' . $this->setting->SETTING_REGIONAL
+        ];
 
-          return view('livewire.homepage.homopage', $this->berita)
-          ->extends('layouts.public', $params)
-          ->section('content');
-      }
+        $data = [
+          $this->berita,
+          'setting' => $this->setting,
+        ];
+
+        return view('livewire.homepage.homopage', $data)
+        ->extends('layouts.public', $params)
+        ->section('content');
+    }
 }
