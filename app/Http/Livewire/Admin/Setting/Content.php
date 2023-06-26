ae5d1ee8fa374ab8b75cf2db8666cca7faee1416
IndraMaulanaPratama <maulana.indra808@gmail.com>
2023-06-26 15:44:03 +0700

    ADD NEW | Handle Request for updating general setting on general setting content

--- app/Http/Livewire/Admin/Setting/Content.php
+++ app/Http/Livewire/Admin/Setting/Content.php
@@ -2,10 +2,42 @@
 
 namespace App\Http\Livewire\Admin\Setting;
 
+use App\Models\setting;
 use Livewire\Component;
 
 class Content extends Component
 {
+    public $setting;
+    public $inputRegional;
+    public $inputDirectorName;
+    public $inputForeword;
+    public $inputPhotoDirector;
+
+    public function mount()
+    {
+        $this->setting = setting::find('1');
+
+        // Configure Form General Setting
+        $this->inputRegional = $this->setting->SETTING_REGIONAL;
+        $this->inputDirectorName = $this->setting->SETTING_DIRECTOR_NAME;
+        $this->inputForeword = $this->setting->SETTING_DIRECTOR_MESSAGE;
+    }
+
+    public function SettingGeneral()
+    {
+
+        try {
+            $this->setting->SETTING_REGIONAL = $this->inputRegional;
+            $this->setting->SETTING_DIRECTOR_NAME = $this->inputDirectorName;
+            $this->setting->SETTING_DIRECTOR_MESSAGE = $this->inputForeword;
+            $this->setting->save();
+            session()->flash('success', 'General Setting Berhasil Diperbaharui');
+        } catch (\Throwable $th) {
+            // session()->flash('error', 'General Setting Gagal Diperbaharui');
+            session()->flash('error', $th->getMessage());
+        }
+    }
+
     public function render()
     {
         return view('livewire.admin.setting.content')
