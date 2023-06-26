c9a0e13e6d931990b52b58096a75efc7db9e6d90
IndraMaulanaPratama <maulana.indra808@gmail.com>
2023-06-26 12:06:57 +0700

    UPDATE | Initiate primary key, add hidden column setting, add default value for director name

--- app/Models/setting.php
+++ app/Models/setting.php
@@ -10,6 +10,7 @@ class setting extends Model
     use HasFactory;
 
     protected $table = 'SETTING';
+    protected $primaryKey = 'SETTING_ID';
 
     public const CREATED_AT = 'SETTING_CREATED_AT';
     public const UPDATED_AT = 'SETTING_UPDATED_AT';
@@ -19,11 +20,21 @@ class setting extends Model
       'SETTING_FAVICON',
       'SETTING_LOGO',
       'SETTING_HEADER',
+      'SETTING_DIRECTOR_NAME',
       'SETTING_DIRECTOR_IMAGE',
       'SETTING_DIRECTOR_MESSAGE',
       'SETTING_URL_MARS',
       'SETTING_URL_HYMNE',
 
+      'SETTING_UPDATED_AT',
+      'SETTING_DELETED_AT',
+
+      'SETTING_CREATED_BY',
+      'SETTING_UPDATED_BY',
+      'SETTING_DELETED_BY',
+    ];
+
+    protected $hidden = [
       'SETTING_UPDATED_AT',
       'SETTING_DELETED_AT',
 
@@ -38,5 +49,6 @@ class setting extends Model
       'SETTING_HEADER' => 'header.jpg',
       'SETTING_URL_MARS' => 'https://youtu.be/Ne-SYQ7GlZw',
       'SETTING_URL_HYMNE' => 'https://youtu.be/7Xro3qZieVQ',
+      'SETTING_DIRECTOR_NAME' => 'Prof. Dr. H. Rama Wirahma, S.T., M.T.',
 ];
 }
