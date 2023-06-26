a474964ed00ccfd81e876d3607126d211a309538
IndraMaulanaPratama <maulana.indra808@gmail.com>
2023-06-26 16:26:48 +0700

    UPDATE || Get Responsive Setting on Logic Marsipdn

--- app/Http/Livewire/Homepage/MarsIpdn.php
+++ app/Http/Livewire/Homepage/MarsIpdn.php
@@ -2,12 +2,21 @@
 
 namespace App\Http\Livewire\Homepage;
 
+use App\Models\setting;
 use Livewire\Component;
 
 class MarsIpdn extends Component
 {
+    public $setting;
+
+    public function mount()
+    {
+        $this->setting = setting::find(1);
+    }
+
     public function render()
     {
-        return view('livewire.homepage.mars-ipdn');
+        $data = ['setting' => $this->setting];
+        return view('livewire.homepage.mars-ipdn', $data);
     }
 }
