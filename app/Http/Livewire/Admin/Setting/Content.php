d36979ceaad9eac5737fbced0447ea29be0df4d7
IndraMaulanaPratama <maulana.indra808@gmail.com>
2023-06-26 16:20:11 +0700

    UPDATE || Edit Hymne and Mars URL on Logic General Setting

--- app/Http/Livewire/Admin/Setting/Content.php
+++ app/Http/Livewire/Admin/Setting/Content.php
@@ -12,6 +12,8 @@ class Content extends Component
     public $inputDirectorName;
     public $inputForeword;
     public $inputPhotoDirector;
+    public $inputMars;
+    public $inputHymne;
 
     public function mount()
     {
@@ -21,16 +23,20 @@ class Content extends Component
         $this->inputRegional = $this->setting->SETTING_REGIONAL;
         $this->inputDirectorName = $this->setting->SETTING_DIRECTOR_NAME;
         $this->inputForeword = $this->setting->SETTING_DIRECTOR_MESSAGE;
+        $this->inputMars = $this->setting->SETTING_URL_MARS;
+        $this->inputHymne = $this->setting->SETTING_URL_HYMNE;
     }
 
     public function SettingGeneral()
     {
-
         try {
             $this->setting->SETTING_REGIONAL = $this->inputRegional;
             $this->setting->SETTING_DIRECTOR_NAME = $this->inputDirectorName;
             $this->setting->SETTING_DIRECTOR_MESSAGE = $this->inputForeword;
+            $this->setting->SETTING_URL_MARS = $this->inputMars;
+            $this->setting->SETTING_URL_HYMNE = $this->inputHymne;
             $this->setting->save();
+
             session()->flash('success', 'General Setting Berhasil Diperbaharui');
         } catch (\Throwable $th) {
             // session()->flash('error', 'General Setting Gagal Diperbaharui');
