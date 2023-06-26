9e763d43470ea4e38417930566df1994d26b6f87
IndraMaulanaPratama <maulana.indra808@gmail.com>
2023-06-26 10:23:32 +0700

    UPDATE | Change Default Header image on public template

--- resources/views/livewire/component/public/header.blade.php
+++ resources/views/livewire/component/public/header.blade.php
@@ -1,5 +1,5 @@
 <div>
     <div class="xl:shadow-xl shadow-lg">
-        <img src="{{ asset('image/banner/header.jpg') }}" alt="" class="xl:h-[67ch] xl:w-[180ch] rounded-md">
+        <img src="https://ipdn.ac.id/assets/img/header/header.jpg" alt="" class="xl:h-[67ch] xl:w-[180ch] rounded-md">
     </div>
 </div>
