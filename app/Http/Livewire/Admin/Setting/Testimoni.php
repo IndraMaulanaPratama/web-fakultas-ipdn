bfe6abd6966ab5606dcd73b081ddd99aedf5ac4d
IndraMaulanaPratama <maulana.indra808@gmail.com>
2023-06-27 11:20:50 +0700

    ADD NEW | Function Responsive show data by testimoni on logic setting testimony

--- app/Http/Livewire/Admin/Setting/Testimoni.php
+++ app/Http/Livewire/Admin/Setting/Testimoni.php
@@ -2,12 +2,43 @@
 
 namespace App\Http\Livewire\Admin\Setting;
 
+use App\Models\testimoni as ModelsTestimoni;
 use Livewire\Component;
 
 class Testimoni extends Component
 {
+    public $testimoni;
+    public $inputId;
+    public $inputName;
+    public $inputBatch;
+    public $inputTestimoni;
+
+
+    public function mount()
+    {
+        $this->testimoni = ModelsTestimoni::all();
+    }
+
+    public function FormEdit($id)
+    {
+        $testimoni = ModelsTestimoni::find($id);
+        $this->inputId = $testimoni->TESTIMONI_ID;
+        $this->inputName = $testimoni->TESTIMONI_USERNAME;
+        $this->inputBatch = $testimoni->TESTIMONI_BATCH;
+        $this->inputTestimoni = $testimoni->TESTIMONI_CONTENT;
+    }
+
     public function render()
     {
-        return view('livewire.admin.setting.testimoni');
+        $params = [
+          'title' => 'Setting Testimoni | IPDN Kampus Deaerah',
+          'page_name' => 'Pengaturan Data Testimoni'
+        ];
+
+        $data = ['data' => $this->testimoni];
+
+        return view('livewire.admin.setting.testimoni', $data)
+          ->extends('layouts.admin', $params)
+          ->section('content');
     }
 }
