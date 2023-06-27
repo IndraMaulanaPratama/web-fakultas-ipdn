0d8ca0b7d90392b6377623f911edaa8112ebf5b1
IndraMaulanaPratama <maulana.indra808@gmail.com>
2023-06-27 16:28:03 +0700

    UPDATE | Responsive Data on Logic Testimoni Homepage

--- app/Http/Livewire/Homepage/Testimoni.php
+++ app/Http/Livewire/Homepage/Testimoni.php
@@ -2,12 +2,28 @@
 
 namespace App\Http\Livewire\Homepage;
 
+use App\Models\setting;
+use App\Models\testimoni as ModelsTestimoni;
 use Livewire\Component;
 
 class Testimoni extends Component
 {
+    public $testimoni;
+    public $setting;
+
+    public function mount()
+    {
+        $this->testimoni = ModelsTestimoni::all();
+        $this->setting = setting::find('1');
+    }
+
     public function render()
     {
-        return view('livewire.homepage.testimoni');
+        $data = [
+          'data' => $this->testimoni,
+          'setting' => $this->setting,
+        ];
+
+        return view('livewire.homepage.testimoni', $data);
     }
 }
