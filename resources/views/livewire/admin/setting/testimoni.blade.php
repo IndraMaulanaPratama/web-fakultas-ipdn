8e702075ec5400010c83ad307c0029e58ed2dac7
IndraMaulanaPratama <maulana.indra808@gmail.com>
2023-06-27 16:07:55 +0700

    REFACTOR | Testimoni Page

--- resources/views/livewire/admin/setting/testimoni.blade.php
+++ resources/views/livewire/admin/setting/testimoni.blade.php
@@ -3,12 +3,10 @@
 
     {{-- Alert for success feedback from backend --}}
     @if (session()->has('success'))
-        <div class="alert alert-primary shadow-lg alert-dismissible fade show" role="alert"">
-            <i class="fe fe-check-circle"></i>
-
-            {{ session('success') }}
-
+        <div class="alert alert-primary shadow-lg alert-dismissible fade show" role="alert">
+            <i class="fe fe-x-circle"></i>
             <span>
+                {{ session('success') }}
                 <button type="button" class="close" data-dismiss="alert" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
@@ -32,56 +30,57 @@
     {{-- Form Edit Testimoni --}}
     <div class="row mb-5">
         <div class="col-md-6">
-            <div class="card shadow">
-                {{-- Card Header --}}
-                <div class="card-header">
-                    <strong class="card-title">{{ __('Edit Data Testimoni ') }}</strong>
-                    <a class="float-right small text-muted" href="#!"> &nbsp; </a>
-                </div>
+            <div class="card shadow" wire:ignore>
+                <form wire:submit.prevent='ProcessUpdate'>
+                    {{-- Card Header --}}
+                    <div class="card-header">
+                        <strong class="card-title">{{ __('Edit Data Testimoni ') }}</strong>
+                        <a class="float-right small text-muted" href="#!"> &nbsp; </a>
+                    </div>
 
-                {{-- Card Body --}}
-                <div class="card-body my-n2">
-                    {{-- Row For Name and Batch Praja --}}
-                    <div class="row">
-                        {{-- Name of Praja --}}
-                        <div class="col-md-8">
-                            <div class="form-group">
-                                <label for="inputName"> {{ __('Nama Praja:') }} </label>
-                                <input type="text" wire:model='inputName' class='form-control'>
+                    {{-- Card Body --}}
+                    <div class="card-body my-n2">
+                        {{-- Row For Name and Batch Praja --}}
+                        <div class="row">
+                            {{-- Name of Praja --}}
+                            <div class="col-md-8">
+                                <div class="form-group">
+                                    <label for="inputName"> {{ __('Nama Praja:') }} </label>
+                                    <input type="text" wire:model='inputName' class='form-control'>
+                                </div>
+                            </div>
+
+                            {{-- Batch Praja --}}
+                            <div class="col-md-4">
+                                <div class="form-group">
+                                    <label for="inputBatch"> {{ __('Angkatan:') }} </label>
+                                    <input type="number" wire:model='inputBatch' min="1956"
+                                        max="{{ date('Y') }}" class='form-control'>
+                                </div>
                             </div>
                         </div>
 
-                        {{-- Batch Praja --}}
-                        <div class="col-md-4">
-                            <div class="form-group">
-                                <label for="inputBatch"> {{ __('Angkatan:') }} </label>
-                                <input type="number" wire:model='inputBatch' min="1956" max="{{ date('Y') }}"
-                                    class='form-control'>
-                            </div>
+                        {{-- Text Testimoni --}}
+                        <div class="form-group">
+                            <label for="inputTestimoni"> {{ __('Testimoni') }} </label>
+                            <textarea wire:model='inputTestimoni' rows="6" class='form-control'></textarea>
                         </div>
+
+                        <input type="hidden" wire:model='inputId' class='form-control'>
+
+                        <hr />
+                        <button type="submit" class="btn btn-info">
+                            {{ __('Simpan Perubahan') }}
+                        </button>
+
+                        &nbsp;
+
+                        <button type="submit" class="btn btn-accent" wire:click='ClearForm'>
+                            {{ __('Batalkan') }}
+                        </button>
                     </div>
-
-                    {{-- Text Testimoni --}}
-                    <div class="form-group">
-                        <label for="inputTestimoni"> {{ __('Testimoni') }} </label>
-                        <textarea wire:model='inputTestimoni' rows="6" class='form-control'></textarea>
-                    </div>
-
-                    <input type="hidden" wire:model='inputId' class='form-control'>
-
-                    <hr />
-                    <button type="submit" class="btn btn-info" wire:click=''>
-                        {{ __('Simpan Perubahan') }}
-                    </button>
-                    &nbsp;
-                    <button type="submit" class="btn btn-accent" wire:click='ClearForm'>
-                      {{ __('Batalkan') }}
-                  </button>
-
-                </div>
-
+                </form>
             </div>
-
         </div>
     </div>
 
