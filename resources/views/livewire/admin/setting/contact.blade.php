0e3b9d69fd9e7d8166cf8045537aa18dffb9a6c2
IndraMaulanaPratama <maulana.indra808@gmail.com>
2023-07-03 10:40:36 +0700

    IMPLEMENT | Responsive Data from Database on Contact Setting Page

--- resources/views/livewire/admin/setting/contact.blade.php
+++ resources/views/livewire/admin/setting/contact.blade.php
@@ -1,6 +1,32 @@
 <div>
     {{-- A good traveler has no fixed plans and is not intent upon arriving. --}}
 
+    {{-- Alert for success feedback from backend --}}
+    @if (session()->has('success'))
+        <div class="alert alert-primary shadow-lg alert-dismissible fade show" role="alert">
+            <i class="fe fe-check-circle"></i>
+            <span>
+                {{ session('success') }}
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </span>
+        </div>
+    @endif
+
+    {{-- Alert for failed or errors feedback from backend --}}
+    @if (session()->has('error'))
+        <div class="alert alert-danger shadow-lg alert-dismissible fade show" role="alert">
+            <i class="fe fe-x-circle"></i>
+            <span>
+                {{ session('error') }}
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </span>
+        </div>
+    @endif
+
     <div class="row">
         <div class="col-md-12">
             <form wire:submit.prevent='SettingContact'>
@@ -14,6 +40,7 @@
                     {{-- Card Body --}}
                     <div class="card-body">
                         <div class="row">
+
                             {{-- Left Side --}}
                             <div class="col-md-6">
                                 {{-- Input Instagram --}}
@@ -34,46 +61,47 @@
                                     <label for="InputEmail"> {{ __('Alamat Surat Elektronik (Email)') }} </label>
 
                                     <div class="input-group mb-3">
-                                        <input type="text" class="form-control" aria-label="Recipient's username"
-                                            aria-describedby="inputEmail" wire:model='inputEmail'>
+                                        <input type="text" class="form-control col-md-6"
+                                            aria-label="Recipient's username" aria-describedby="inputEmail"
+                                            wire:model='inputEmail'>
                                         <div class="input-group-append">
                                             <span class="input-group-text" id="inputEmail">@ipdn.ac.id</span>
                                         </div>
                                     </div>
                                 </div>
 
-                                {{-- Input Telephone --}}
+                                {{-- Input Phone --}}
                                 <div class="form-group">
-                                    <label for="inputTelephone"> {{ __('Nomor Telepon') }} </label>
-                                    <input type="number" class="form-control" wire:model='inputTelephone'>
+                                    <label for="inputPhone"> {{ __('Nomor Telepon') }} </label>
+                                    <input class="form-control col-md-4 input-phoneus" wire:model='inputPhone'>
                                 </div>
 
                                 {{-- Input Fax --}}
                                 <div class="form-group">
                                     <label for="inputFax"> {{ __('Nomor Fax') }} </label>
-                                    <input type="number" class="form-control" wire:model='inputFax'>
+                                    <input class="form-control col-md-4 input-phoneus" wire:model='inputFax'>
                                 </div>
-
                             </div>
 
                             {{-- Right Side --}}
                             <div class="col-md-6">
                                 {{-- Input Address --}}
                                 <div class="form-group">
-                                    <label for="inputAddress">Alamat Lengkap</label>
+                                    <label for="inputAddress">{{ __('Alamat Lengkap') }}</label>
                                     <textarea class='form-control' rows="3" wire:model='inputAddress'></textarea>
                                 </div>
 
                                 {{-- Input Maps --}}
                                 <div class="form-group">
-                                    <label for="inputAddress">Tautan Maps</label>
+                                    <label for="inputMaps">{{ __('Tautan Maps') }}</label>
                                     <textarea class='form-control' rows="3" wire:model='inputMaps'></textarea>
                                 </div>
 
                                 {{-- Buttton Submit --}}
                                 &nbsp;
-                                <button type="submit" class="btn btn-lg btn-secondary form-control">Simpan
-                                    Perubahan</button>
+                                <button type="submit" class="btn btn-lg btn-secondary form-control">
+                                    {{ __('Simpan Perubahan') }}
+                                </button>
                             </div>
                         </div>
 
