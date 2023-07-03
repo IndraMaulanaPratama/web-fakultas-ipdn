b3d3586dbe0a05b379d1a36de96ae4e3c2a13f3d
IndraMaulanaPratama <maulana.indra808@gmail.com>
2023-07-03 10:38:29 +0700

    ADD NEW | Handle Request Update Data on Logic Contact Setting

--- app/Http/Livewire/Admin/Setting/Contact.php
+++ app/Http/Livewire/Admin/Setting/Contact.php
@@ -8,10 +8,40 @@ use Livewire\Component;
 class Contact extends Component
 {
     public $contact;
+    public $inputInstagram;
+    public $inputEmail;
+    public $inputPhone;
+    public $inputFax;
+    public $inputAddress;
+    public $inputMaps;
 
     public function mount()
     {
         $this->contact = ModelsContact::find(1);
+        $this->inputInstagram = $this->contact->CONTACT_INSTAGRAM;
+        $this->inputEmail = $this->contact->CONTACT_EMAIL;
+        $this->inputPhone = $this->contact->CONTACT_TELEPHONE;
+        $this->inputFax = $this->contact->CONTACT_FAX;
+        $this->inputAddress = $this->contact->CONTACT_ADDRESS;
+        $this->inputMaps = $this->contact->CONTACT_MAPS;
+    }
+
+    public function SettingContact()
+    {
+        try {
+
+            $this->contact->CONTACT_INSTAGRAM = $this->inputInstagram;
+            $this->contact->CONTACT_EMAIL = $this->inputEmail;
+            $this->contact->CONTACT_TELEPHONE = $this->inputPhone;
+            $this->contact->CONTACT_FAX = $this->inputFax;
+            $this->contact->CONTACT_ADDRESS = $this->inputAddress;
+            $this->contact->CONTACT_MAPS = $this->inputMaps;
+            $this->contact->save();
+
+            session()->flash('success', 'Pengaturan Kontak Berhasil Diperbarui');
+        } catch (\Throwable $th) {
+            session()->flash('error', 'Pengaturan Kontak Gagal Diperbarui');
+        }
     }
 
     public function render()
