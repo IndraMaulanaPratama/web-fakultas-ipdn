0b183c00418d64069b5c0e7637d9b66c3b0bb8a0
IndraMaulanaPratama <maulana.indra808@gmail.com>
2023-07-03 08:34:25 +0700

    ADD NEW || Get Data Article From Database on Logic List Berita

--- app/Http/Livewire/ListBerita/ListBerita.php
+++ app/Http/Livewire/ListBerita/ListBerita.php
@@ -2,17 +2,33 @@
 
 namespace App\Http\Livewire\ListBerita;
 
+use App\Models\article;
 use Livewire\Component;
 
 class ListBerita extends Component
 {
+    public $berita;
+    public $countArticle;
+
+    protected $count =  5;
+
+    public function mount()
+    {
+        $this->berita = article::take($this->countArticle)->nullDeleted()->get();
+        dd($this->berita);
+    }
+
     public function render()
     {
         $params = [
             'title' => 'List Berita - IPDN Kampus Papua'
         ];
 
-        return view('livewire.list-berita.list-berita')
+        $data = [
+          'data' => $this->berita,
+        ];
+
+        return view('livewire.list-berita.list-berita', $data)
         ->extends('layouts.public', $params)
         ->section('content');
     }
