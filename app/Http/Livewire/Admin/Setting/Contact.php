cfa9dfdbf07a018832379ec93650332b58efc6cb
IndraMaulanaPratama <maulana.indra808@gmail.com>
2023-07-03 09:32:44 +0700

    ADD NEW | Get Data from database on Logic Setting Contact

--- app/Http/Livewire/Admin/Setting/Contact.php
+++ app/Http/Livewire/Admin/Setting/Contact.php
@@ -2,12 +2,30 @@
 
 namespace App\Http\Livewire\Admin\Setting;
 
+use App\Models\contact as ModelsContact;
 use Livewire\Component;
 
 class Contact extends Component
 {
+    public $contact;
+
+    public function mount()
+    {
+        $this->contact = ModelsContact::find(1);
+    }
+
     public function render()
     {
-        return view('livewire.admin.setting.contact');
+        $params = [
+          'title' => 'Pengaturan Kontak - IPDN Kampus Daerah',
+          'page_name' => 'Halaman Pengaturan Kontak'
+        ];
+
+        $data = [
+          'data' => $this->contact,
+        ];
+        return view('livewire.admin.setting.contact', $data)
+        ->extends('layouts.admin', $params)
+        ->section('content');
     }
 }
