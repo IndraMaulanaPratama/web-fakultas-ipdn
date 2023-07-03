1b0078aef8e71372143ec59f570bb38ecd5a1275
IndraMaulanaPratama <maulana.indra808@gmail.com>
2023-07-03 10:59:58 +0700

    ADD NEW | Get Data Responsive From Database on Logic Footer

--- app/Http/Livewire/Component/Public/Footer.php
+++ app/Http/Livewire/Component/Public/Footer.php
@@ -2,12 +2,20 @@
 
 namespace App\Http\Livewire\Component\Public;
 
+use App\Models\contact;
 use Livewire\Component;
 
 class Footer extends Component
 {
+    public $contact;
+
+    public function mount()
+    {
+        $this->contact = contact::find(1);
+    }
+
     public function render()
     {
-        return view('livewire.component.public.footer');
+        return view('livewire.component.public.footer', ['data' => $this->contact]);
     }
 }
