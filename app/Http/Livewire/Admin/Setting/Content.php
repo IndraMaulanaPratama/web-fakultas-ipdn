2d875aea9dc68d3516c7b4eef9af39b1c6988638
IndraMaulanaPratama <maulana.indra808@gmail.com>
2023-07-07 09:53:57 +0700

    IMPLEMENT | Realtime Validation and Upload Photo on Logic General Setting

--- app/Http/Livewire/Admin/Setting/Content.php
+++ app/Http/Livewire/Admin/Setting/Content.php
@@ -4,9 +4,12 @@ namespace App\Http\Livewire\Admin\Setting;
 
 use App\Models\setting;
 use Livewire\Component;
+use Livewire\WithFileUploads;
 
 class Content extends Component
 {
+    use WithFileUploads;
+
     public $setting;
     public $inputRegional;
     public $inputDirectorName;
@@ -14,6 +17,15 @@ class Content extends Component
     public $inputPhotoDirector;
     public $inputMars;
     public $inputHymne;
+
+    protected $rules = [
+      'inputRegional' => ['required', 'max:200'],
+      'inputDirectorName' => ['required', 'max:150'],
+      'inputForeword' => ['required', 'max:255'],
+      'inputPhotoDirector' => ['image', 'max:50'],
+      'inputMars' => ['required'],
+      'inputHymne' => ['required'],
+    ];
 
     public function mount()
     {
@@ -27,15 +39,36 @@ class Content extends Component
         $this->inputHymne = $this->setting->SETTING_URL_HYMNE;
     }
 
+    // Realtime Form Validation
+    public function updated($propertyName)
+    {
+        $this->validateOnly($propertyName);
+    }
+
     public function SettingGeneral()
     {
+        // Get photo name
+        $filename = $this->inputPhotoDirector->getClientOriginalName();
+
+        // Initiate Field and value
+        $value = [
+          $this->setting->SETTING_REGIONAL = $this->inputRegional,
+          $this->setting->SETTING_DIRECTOR_NAME = $this->inputDirectorName,
+          $this->setting->SETTING_DIRECTOR_MESSAGE = $this->inputForeword,
+          $this->setting->SETTING_DIRECTOR_IMAGE = $filename,
+          $this->setting->SETTING_URL_MARS = $this->inputMars,
+          $this->setting->SETTING_URL_HYMNE = $this->inputHymne,
+        ];
+
         try {
-            $this->setting->SETTING_REGIONAL = $this->inputRegional;
-            $this->setting->SETTING_DIRECTOR_NAME = $this->inputDirectorName;
-            $this->setting->SETTING_DIRECTOR_MESSAGE = $this->inputForeword;
-            $this->setting->SETTING_URL_MARS = $this->inputMars;
-            $this->setting->SETTING_URL_HYMNE = $this->inputHymne;
-            $this->setting->save();
+            // Upload Photo
+            $this->inputPhotoDirector->storeAS(
+                'director',
+                $filename,
+                'dir_image'
+            );
+
+            $this->setting->save($value);
 
             session()->flash('success', 'General Setting Berhasil Diperbaharui');
         } catch (\Throwable $th) {
@@ -50,10 +83,11 @@ class Content extends Component
         ->extends(
             'layouts.admin',
             [
-            'title' => 'Pengaturan General - IPDN Kampus Daerah',
-            'page_name' => 'Pengaturan General'
-          ]
+          'title' => 'Pengaturan General - IPDN Kampus Daerah',
+          'page_name' => 'Pengaturan General'
+        ]
         )
-        ->section('content');
+      ->section('content');
+
     }
 }
