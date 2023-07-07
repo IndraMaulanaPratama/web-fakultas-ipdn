9f2cb336572a1c7f8008e8ce35b5db5fb3e19524
IndraMaulanaPratama <maulana.indra808@gmail.com>
2023-07-07 10:34:38 +0700

    IMPLEMENT | Function Upload Photo on Logic Setting Testimoni

--- app/Http/Livewire/Admin/Setting/Testimoni.php
+++ app/Http/Livewire/Admin/Setting/Testimoni.php
@@ -4,22 +4,42 @@ namespace App\Http\Livewire\Admin\Setting;
 
 use App\Models\testimoni as ModelsTestimoni;
 use Livewire\Component;
+use Livewire\WithFileUploads;
 
 class Testimoni extends Component
 {
+    use WithFileUploads;
+
     public $testimoni;
     public $inputId;
     public $inputName;
     public $inputBatch;
     public $inputTestimoni;
+    public $inputPhoto;
 
     protected $listeners = ['refreshComponent' => '$refresh'];
+
+    // Rules of Form Validation
+    protected $rules = [
+      'inputId' => ['required'],
+      'inputName' => ['required', 'max:100'],
+      'inputBatch' => ['required', 'max:4'],
+      'inputTestimoni' => ['required', 'max:255'],
+      'inputPhoto' => ['image', 'max:255'],
+    ];
 
     public function mount()
     {
         $this->testimoni = ModelsTestimoni::all();
     }
 
+    // Reatime Form Validation
+    public function updated($propertyName)
+    {
+        $this->validateOnly($propertyName);
+    }
+
+    // Updating Form Value Based On Choosen Data
     public function FormEdit($id)
     {
         $testimoni = ModelsTestimoni::find($id);
@@ -29,30 +49,63 @@ class Testimoni extends Component
         $this->inputTestimoni = $testimoni->TESTIMONI_CONTENT;
     }
 
+    // Set Null Value for All of Input Form
     public function ClearForm()
     {
         $this->inputId = null;
         $this->inputName = null;
         $this->inputBatch = null;
         $this->inputTestimoni = null;
+        $this->inputPhoto = null;
+        $this->emit('refreshComponent');
     }
 
     public function ProcessUpdate()
     {
+        $this->validate();
+
         $dataTestimoni = ModelsTestimoni::find($this->inputId);
 
+        // Initiate Field and Value
+        if (null == $this->inputPhoto):
+            $value = [
+              $dataTestimoni->TESTIMONI_USERNAME = $this->inputName,
+              $dataTestimoni->TESTIMONI_BATCH = $this->inputBatch,
+              $dataTestimoni->TESTIMONI_CONTENT = $this->inputTestimoni,
+            ];
+
+            // When admin changes photo
+        else:
+            // Get Photos Name
+            $filename = $this->inputPhoto->getClientOriginalName();
+
+            $value = [
+              $dataTestimoni->TESTIMONI_USERNAME = $this->inputName,
+              $dataTestimoni->TESTIMONI_BATCH = $this->inputBatch,
+              $dataTestimoni->TESTIMONI_CONTENT = $this->inputTestimoni,
+              $dataTestimoni->TESTIMONI_IMAGE = $filename,
+            ];
+
+            // Process Upload File to Fiilesystem dir_image
+            $this->inputPhoto->storeAs(
+                'testimoni',
+                $filename,
+                'dir_image'
+            );
+        endif;
+
+        // Logic Process Updating Data
         try {
-            $dataTestimoni->TESTIMONI_USERNAME = $this->inputName;
-            $dataTestimoni->TESTIMONI_BATCH = $this->inputBatch;
-            $dataTestimoni->TESTIMONI_CONTENT = $this->inputTestimoni;
-            $dataTestimoni->save();
+            $dataTestimoni->save($value);
 
             $this->ClearForm();
             $this->emit('refreshComponent');
-            session()->flash('success', 'Data Testimoni Berhasil Perbarui');
+            session()->flash('success', 'Data Testimoni Berhasil di Perbarui');
         } catch (\Throwable $th) {
-            session()->flash('error', $th->getMessage());
+            session()->flash('error', 'Data Testimoni Gagal di Perbarui');
+            // session()->flash('error', $th->getMessage());
         }
+
     }
 
     public function render()
