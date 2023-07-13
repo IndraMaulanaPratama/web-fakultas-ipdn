057733136013e2c06b48408804df737d5b9d354b
IndraMaulanaPratama <maulana.indra808@gmail.com>
2023-07-13 16:07:01 +0700

    UPDATE | Responsive Form Add Menu and Sub Menu on Menu Page

--- resources/views/livewire/admin/menu/main.blade.php
+++ resources/views/livewire/admin/menu/main.blade.php
@@ -27,7 +27,7 @@
     @endif
 
     <div class="row">
-        <div class="col-md-12">
+        <div class="col-xl-9">
             <div class="card show">
 
                 {{-- Header Card Data Menu --}}
@@ -72,108 +72,13 @@
                             </div>
 
                             {{-- Separated --}}
-                            <div class="col-xl-5">&nbsp;</div>
+                            <div class="col-xl-4">&nbsp;</div>
 
                             {{-- Button add New --}}
-                            <div class="col-xl-1">
-                                <button type="button" class="btn mb-2 btn-secondary" data-toggle="modal"
-                                    data-target=".modalAddNew"> {{ __('Tambah Menu') }} </button>
-
-                                <!-- Modal Add Data -->
-                                <div class="modal fade modalAddNew modal-slide" tabindex="-1" role="dialog"
-                                    aria-labelledby="defaultModalLabel" aria-hidden="true">
-                                    <div class="modal-dialog modal-sm" role="document">
-                                        <div class="modal-content">
-
-                                            {{-- Header Modal --}}
-                                            <div class="modal-header">
-
-                                                {{-- Title --}}
-                                                <h5 class="modal-title" id="defaultModalLabel">
-                                                    Form Tambah Data Menu
-                                                </h5>
-
-                                                {{-- Close Button --}}
-                                                <button type="button" class="close" data-dismiss="modal"
-                                                    aria-label="Close">
-                                                    <span aria-hidden="true">&times;</span>
-                                                </button>
-                                            </div>
-
-                                            {{-- Body Modal --}}
-                                            <div class="modal-body">
-                                                <form wire:submit.prevent='TambahData'>
-
-                                                    {{-- Menu Name --}}
-                                                    <div class="form-group">
-                                                        <label for="inputName"> {{ __('Nama') }}:</label>
-                                                        <input type="text" wire:model='inputName'
-                                                            class='form-control'>
-                                                    </div>
-
-                                                    {{-- Orders --}}
-                                                    <div class="form-group">
-                                                        <label for="inputOrder"> {{ __('Nomor Urut') }}:</label>
-                                                        <input type="number" wire:model='inputOrder'
-                                                            class='form-control col-md-4'>
-                                                    </div>
-
-                                                    {{-- URL --}}
-                                                    <div class="form-group">
-                                                        <label for="inputUrl"> {{ __('Tujuan URL') }}:</label>
-                                                        <div class="input-group mb-3">
-                                                            <div class="input-group-prepend">
-                                                                <span class="input-group-text" id="basic-addon3">
-                                                                    {{ env('APP_URL') }} </span>
-                                                            </div>
-                                                            <input type="text" wire:model='inputUrl'
-                                                                class="form-control" id="basic-url"
-                                                                aria-describedby="basic-addon3">
-                                                        </div>
-                                                    </div>
-
-                                                    {{-- Orders --}}
-                                                    <div class="form-group">
-                                                        <label for="inputIcon"> {{ __('Icon Menu') }}:</label>
-                                                        <input type="file" wire:model='inputIcon'
-                                                            class='form-control'>
-                                                    </div>
-
-                                                    {{-- Divider --}}
-                                                    <div class="my-4">
-                                                        <hr>
-                                                    </div>
-
-                                                    {{-- Button --}}
-                                                    <div>
-                                                        {{-- Close Button --}}
-                                                        <button type="button" class="btn mb-2 btn-outline"
-                                                            data-dismiss="modal">
-                                                            {{ __('Batalkan') }}
-                                                        </button>
-                                                        {{-- Save Button --}}
-                                                        <button type="button" class="btn mb-2 btn-secondary">
-                                                            {{ __('Simpan Data') }}
-                                                        </button>
-                                                    </div>
-
-                                                </form>
-                                            </div>
-
-
-                                            {{-- Footer Modal --}}
-                                            <div class="modal-footer">
-                                                {{-- Close Button --}}
-                                                {{-- <button type="button" class="btn mb-2 btn-secondary"
-                                                    data-dismiss="modal">Close</button> --}}
-
-                                                {{-- Save Button --}}
-                                                {{-- <button type="button" class="btn mb-2 btn-primary">Save
-                                                    changes</button> --}}
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
+                            <div class="col-xl-2">
+                                <button type="button" class="btn mb-2 btn-secondary" wire:click="ShowAddMenu">
+                                    {{ __('Tambah Menu') }}
+                                </button>
                             </div>
 
                         </div>
@@ -211,7 +116,7 @@
                                                 <div class="dropdown-menu dropdown-menu-right">
                                                     <a class="dropdown-item" href="#">Ubah</a>
                                                     <a class="dropdown-item" href="#">Hapus</a>
-                                                    <a class="dropdown-item" href="#">Tambahkan Submenu</a>
+                                                    <a class="dropdown-item" wire:click="ShowAddSubMenu">Tambahkan Submenu</a>
                                                 </div>
                                             </td>
                                         </tr>
@@ -282,14 +187,13 @@
                 </div>
             </div>
         </div>
-    </div>
 
-    {{-- Modal Form Add New --}}
-    <dialog id="modalAddNew" class="modal">
-        <form method="dialog" class="modal-box">
-            <button class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
-            <h3 class="font-bold text-lg">Hello!</h3>
-            <p class="py-4">Press ESC key or click on ✕ button to close</p>
-        </form>
-    </dialog>
-</div>
+        <div class="col-xl-3">
+            @if ('add-menu' == $formSetting)
+                @livewire('component.admin.menu.add-menu')
+            @elseif ('add-sub-menu' == $formSetting)
+                @livewire('component.admin.menu.add-sub-menu')
+            @endif
+        </div>
+
+    </div>
