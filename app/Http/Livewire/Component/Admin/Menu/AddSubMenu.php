bbc11aa256d01810b52eb3c8ebeacfb43c8c82a1
IndraMaulanaPratama <maulana.indra808@gmail.com>
2023-07-13 16:08:05 +0700

    ADD NEW | Logic For Handler Add Sub Menu

---
+++ app/Http/Livewire/Component/Admin/Menu/AddSubMenu.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Component\Admin\Menu;
+
+use Livewire\Component;
+
+class AddSubMenu extends Component
+{
+    public function render()
+    {
+        return view('livewire.component.admin.menu.add-sub-menu');
+    }
+}
