4581025db8294f85724d51513f4314d819a51a5c
IndraMaulanaPratama <maulana.indra808@gmail.com>
2023-07-13 16:08:48 +0700

    ADD NEW | Function for handler responsive form add menu and submenu on menu logic

--- app/Http/Livewire/Admin/Menu/Main.php
+++ app/Http/Livewire/Admin/Menu/Main.php
@@ -3,9 +3,46 @@
 namespace App\Http\Livewire\Admin\Menu;
 
 use Livewire\Component;
+use Livewire\WithFileUploads;
 
 class Main extends Component
 {
+    use WithFileUploads;
+
+    public $formSetting = null;
+
+    protected $rules = [
+      'inputName' => ['required', 'max:20'],
+      'inputOrder' => ['required', 'digits_between:1,99'],
+      'inputUrl' => ['required', 'max:255'],
+      'inputIcon' => ['image'],
+    ];
+
+    protected $listeners = [
+      'close-form' => 'CloseForm',
+    ];
+
+    public function updated($variableName)
+    {
+        $this->validateOnly($variableName);
+    }
+
+    public function ShowAddMenu()
+    {
+        $this->formSetting = 'add-menu';
+    }
+
+    public function ShowAddSubMenu()
+    {
+        $this->formSetting = 'add-sub-menu';
+    }
+
+    public function CloseForm()
+    {
+        $this->formSetting = null;
+    }
+
+
     public function render()
     {
         $params = [
