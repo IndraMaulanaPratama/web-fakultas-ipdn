d1f95eb5b763f72459119d987fd6889d983fefdb
IndraMaulanaPratama <maulana.indra808@gmail.com>
2023-07-13 16:07:33 +0700

    ADD NEW | Form Add Sub Menu Page

---
+++ resources/views/livewire/component/admin/menu/add-sub-menu.blade.php
@@ -0,0 +1,7 @@
+<div>
+    {{-- Because she competes with no one, no one can compete with her. --}}
+    <div class="card show">
+      <div class="card-header"></div>
+      <div class="card-body">ini form update</div>
+    </div>
+</div>
